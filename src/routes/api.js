abf49ac307ae6d93c1fdbd6911215babbc7416cf
Cristopher Said Romero Juarez <cristophersaidromerojuarez@Mac-mini-de-Cristopher.local>
2025-11-25 12:01:04 -0600

    WIP: Fix month stats route order and add custom calendar CSS

--- src/routes/api.js
+++ src/routes/api.js
@@ -11,10 +11,10 @@ router.get('/services', ServicesController.getAll);
 
 // Appointments
 router.post('/appointments', AppointmentsController.create);
-router.get('/appointments', AppointmentsController.getByDate);
 router.get('/appointments/month', AppointmentsController.getByMonth);
 router.patch('/appointments/:id/cancel', AppointmentsController.cancel);
 router.get('/appointments/:id', AppointmentsController.getById);
 router.patch('/appointments/:id', AppointmentsController.update);
+router.get('/appointments', AppointmentsController.getByDate);
 
 export default router;
